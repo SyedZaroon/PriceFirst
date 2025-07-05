4e7c85ba2c4974e67a41e320957d9d4a342135d3
Zaroon ali <zaroonalichishti@gmail.com>
2025-07-06 00:49:09 +0500

    working on thankyou page

--- src/components/iconBox/IconBox.jsx
+++ src/components/iconBox/IconBox.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./iconBox.module.css";
 import SecuritySafe from "../../assets/icons/SecuritySafe";
+import Danger from "../../assets/icons/Danger";
+import clsx from "clsx";
 
 const IconBox = ({
   title = "Safe & Secure",
   desc = "Your information is securely passed to the selected buyer, we never share your data.",
   icon = <SecuritySafe />,
+  variant = "",
 }) => {
   return (
-    <div className={styles["iconbox"]}>
-      <h4 className={styles["iconbox-title"]}>{title}</h4>
-      <p className={styles["iconbox-desc"]}>{desc}</p>
+    <div className={clsx(styles["iconbox"], styles[`${variant}-iconbox`])}>
+      { variant?  <Danger svgClass={styles.danger} /> : ""}
+      <div className={styles["iconbox-content"]} >
+        <h4 className={styles["iconbox-title"]}>{title}</h4>
+        <p className={styles["iconbox-desc"]}>{desc}</p>
+      </div>
       <span className={styles.icon}>
         {React.cloneElement(icon, {
           svgClass: styles.svg,
--- src/pages/thankYouPage/ThankYouPage.jsx
+++ src/pages/thankYouPage/ThankYouPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import styles from './thankYouPage.module.css'
 import OrderDetails from '@/components/orderDetails/OrderDetails';
+import IconBox from "@/components/iconBox/IconBox";
 
 
 const ThankYouPage = () => {
   return (
     <>
       <div className={styles["thankyou-box"]}>
-              <h1 className={styles["thankyou-heading"]} >Thank you for your order!</h1>
-              <OrderDetails />
+        <div>
+          <h1 className={styles["thankyou-heading"]}>
+            Thank you for your order!
+          </h1>
+          <OrderDetails />
+          <div>
+            <IconBox
+              title="Please remove your iCloud account before sending your device"
+              desc="PhoneBox can only offer a non-working price for devices locked to an iCloud account. Please remove your iCloud account before sending your device to PhoneBox."
+              variant="disclaimer"
+            />
+          </div>
+        </div>
+        <div>
+          <h1 className={styles["thankyou-heading"]}>What to do next?</h1>
+        </div>
       </div>
     </>
   );
