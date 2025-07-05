60a66fb7d07788fa4dca76df187fd254d1572eef
Zaroon ali <zaroonalichishti@gmail.com>
2025-07-05 19:12:18 +0500

    contact page design

--- src/Pages/contactPage/ContactUs.jsx
+++ src/Pages/contactPage/ContactUs.jsx
@@ -1,12 +1,52 @@
 import React from 'react'
-import PageMenu from '../../components/pageMenu/PageMenu'
-
+import styles from './contactUs.module.css'
+import Inputfields from '@/components/inputFields/InputFields'
+import Button from '@/components/button/Button'
 const ContactUs = () => {
   return (
     <>
-      <PageMenu />
+      <div className={styles["contact-banner"]}>
+        <h1>
+          <span>Say Hello!</span> Got a question or a device to sell?
+        </h1>
+        <p>Got a question or a device to sell?</p>
+      </div>
+
+      <div className={styles["form-box"]}>
+        <form className={styles["contact-form"]} >
+          <div className={styles["form-row1"]}>
+            <Inputfields
+              className="styles[contactForm-field]"
+              label="Name*"
+              variant="underline"
+              placeholder="Let us know who you are."
+              type="text"
+            />
+
+            <Inputfields
+              className="styles[contactForm-field]"
+              label="Email*"
+              variant="underline"
+              placeholder="Where should we reply?"
+              type="email"
+            />
+          </div>
+
+          <Inputfields
+            className="styles[contactForm-field]"
+            label="Message*"
+            variant="underline"
+            placeholder="Ask us anything or share your thoughts."
+            type="text"
+          />
+
+          <Button className={styles["contact-submit-btn"]} variant="primary" size="md">
+            Send Message
+          </Button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
 export default ContactUs
